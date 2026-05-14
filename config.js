8e6de2b81667d080e2d6ef9e8dfc6407b3fc6903
estebanzu <72096773+estebanzu@users.noreply.github.com>
2026-05-14 12:18:51 -0600

    add external configuration CONFIG.JS

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: "https://cksfawqsswhqqzkajkkn.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrc2Zhd3Fzc3docXF6a2Fqa2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzM0ODAsImV4cCI6MjA5NDM0OTQ4MH0.tRx76OrfJ04GwVVNtcnF62kH45Uv-IksOebeSXWVEo8"
+};
